71466077ae61d885a1e808512b04d665f509dc18
Domingo Salvucci <salvuccid@gmail.com>
2022-07-19 15:05:25 -0300

    Subir correcion de Karina

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+// const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav-ul");
+
 
 navToggle.addEventListener('click', () => {
    navMenu.classList.toggle("nav-menu_visible");
