4b309c8483116000a918004e4633a080864d5594
Domingo Salvucci <salvuccid@gmail.com>
2022-07-22 17:36:58 -0300

    Desafio Clase 11 Frameworks CSS + Bootstrap

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
-const navToggle = document.querySelector(".nav-toggle");
+// const navToggle = document.querySelector(".nav-toggle");
+// // const navMenu = document.querySelector(".nav-menu");
+// const navMenu = document.querySelector(".nav-ul");
+
+// navToggle.addEventListener('click', () => {
+//    navMenu.classList.toggle("nav-menu_visible");
+// });
+
+const navToggle_pp = document.querySelector(".nav-toggle_pp");
 // const navMenu = document.querySelector(".nav-menu");
-const navMenu = document.querySelector(".nav-ul");
+const navMenu_pp = document.querySelector(".nav-ul_pp");
 
-
-navToggle.addEventListener('click', () => {
-   navMenu.classList.toggle("nav-menu_visible");
+navToggle_pp.addEventListener('click', () => {
+   navMenu_pp.classList.toggle("nav-menu_visible");
 });
+
